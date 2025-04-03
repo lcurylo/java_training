2d41a94f79d4fa7bd7d2f040cf6212dc3c93acfa
lcurylo <lidia.kaczma@gmail.com>
2025-04-03 12:00:53 +0200

    Hello World example with Exercise Hello World

---
+++ src/main/java/pl/javastart/courses/HelloWorld.java
@@ -0,0 +1,12 @@
+package pl.javastart.courses;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.print("Hello and welcome!");
+
+
+    }
+}
---
+++ src/main/java/pl/javastart/exercise/ExerciseHelloWorld.java
@@ -0,0 +1,9 @@
+package pl.javastart.exercise;
+
+public class ExerciseHelloWorld {
+
+    public static void main(String args) {
+        System.out.print("Simple output");
+
+    }
+}
